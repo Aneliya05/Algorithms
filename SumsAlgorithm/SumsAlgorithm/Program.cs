0810ccc0b033d33b6cf28a9efff628a52acc945f
Aneliya05 <89069514+Aneliya05@users.noreply.github.com>
2023-09-16 00:53:42 +0300

    SumsAlrorithm
    This is an algorithm that can help you find the best way to pay a sum with the smallest possible quantity

---
+++ SumsAlgorithm/SumsAlgorithm/Program.cs
@@ -0,0 +1,82 @@
+﻿namespace SumsAlrorithm
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            SortedDictionary<int, int> availableCoins = new SortedDictionary<int, int>();
+            Console.Write("How many types of coins do you have? ");
+            int coinsCount = int.Parse(Console.ReadLine());
+            for (int i = 0; i < coinsCount; i++)
+            {
+                Console.Write("Enter value of a coin: ");
+                int value = int.Parse(Console.ReadLine());
+                Console.Write($"Enter the count of the coin with value {value}: ");
+                int count = int.Parse(Console.ReadLine());
+                availableCoins.Add(value, count);
+            }
+
+            Console.WriteLine();
+            Console.Write("Enter the total sum: ");
+            int sum = int.Parse(Console.ReadLine());
+
+            Dictionary<int, int> result = new Dictionary<int, int>();
+
+            int remainingSum = sum;
+            int sumOfCoins = 0;
+            CheckForEnoughCoins(availableCoins, sumOfCoins, sum);
+
+            foreach (var value in availableCoins.Keys.Reverse())
+            {
+                int coinCount = remainingSum / value;
+                int availableCount = availableCoins[value];
+                sumOfCoins = sumOfCoins + value * availableCount;
+
+                
+                if (coinCount <= availableCount)
+                {
+                    result.Add(value, coinCount);
+                    remainingSum = remainingSum - coinCount * value;
+                    int newAvailableCount = availableCount - coinCount;
+                    availableCoins[value] = newAvailableCount;
+                }
+                else
+                {
+                    result.Add(value, availableCount);
+                    remainingSum = remainingSum - availableCount * value;
+                }
+
+                if (remainingSum == 0)
+                {
+                    PrintCoins(result);
+                    break;
+                }
+
+            }
+        }
+        public static void PrintCoins(Dictionary<int, int> result)
+        {
+            foreach (var pair in result)
+            {
+                Console.WriteLine("{0} -> {1}", pair.Key, pair.Value);
+            }
+
+        }
+        public static void CheckForEnoughCoins(SortedDictionary<int, int> availableCoins, int sumOfCoins, int sum)
+        {
+            foreach (var value in availableCoins.Keys.Reverse())
+            {
+                int coinCount = sum / value;
+                int availableCount = availableCoins[value];
+                sumOfCoins = sumOfCoins + value * availableCount;
+            }
+            if(sumOfCoins < sum)
+            {
+                Console.WriteLine("You do not have enough coins!");
+            }
+        }
+    }
+}
+
+
+
